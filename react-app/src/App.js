719e461422710faacc8e05ff12e18b34c46e109f
Himanshu <himanshudodiya26@gmail.com>
2023-03-01 00:42:53 +0530

    react-app folder added

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Greet from "./components/greet";
 import Welcome from "./components/welcome";
 import Message from"./components/message";
+import Counter from './components/counter';
 // function App() {
 //   return (
 //     <div className="App">
@@ -23,7 +24,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet name="Bruce" heroName="batman">
+        {/* <Greet name="Bruce" heroName="batman">
           <p></p>
         </Greet>
         <Greet name="Clark" heroName="supername"></Greet>
@@ -31,8 +32,8 @@ class App extends Component {
         <Welcome name="Bruce" heroName="batman"></Welcome>
         <Welcome name="Clark" heroName="superman"></Welcome>
         <Welcome name="Diana" heroName="wonder woman"></Welcome>
-        <Message></Message>
-
+        <Message></Message> */}
+<Counter></Counter>
       </div>
     );
   }
